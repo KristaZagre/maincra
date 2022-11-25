43f3b1dc21e207a877c1383a5429615bb95d8af3
Ola Stenberg <hello@olastenberg.com>
2022-11-25 17:05:35 +0100

    test(apps/furo): init playwright

--- apps/furo/test/pages/index.test.ts
+++ apps/furo/test/pages/index.test.ts
@@ -6,15 +6,49 @@ if (!process.env.CHAIN_ID) {
 
 test.beforeEach(async ({ page }) => {
   await page.goto(process.env.PLAYWRIGHT_URL as string)
-})
-
-test('Create a stream', async ({ page }) => {
-  test.slow()
   page.on('pageerror', (err) => {
     console.log(err.message)
   })
-  const createStreamLink = page.locator('[testdata-id=create-stream-button]')
-  createStreamLink.click()
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(process.env.PLAYWRIGHT_URL + '/stream/create')
 })
+
+
+
+test.describe('Create stream', () => {
+
+  test('Create a stream', async ({ page }) => {
+ 
+    const createStreamLink = page.locator('[testdata-id=create-stream-button]')
+    await createStreamLink.click()
+    
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(process.env.PLAYWRIGHT_URL + '/stream/create')
+  })
+
+  
+  // test('Create a stream using bentobox balance', async ({ page }) => {
+ 
+  // })
+
+})
+
+
+
+// test.describe('Create vest', () => {
+
+//   test('Graded', async() => {
+
+//   })
+
+//   test('Cliff', async() => {
+
+//   })
+
+//   test('Hybrid', async() => {
+
+//   })
+
+// })
+
+// test.describe('Create vest with balance from bentobox', () => {
+
+// })
