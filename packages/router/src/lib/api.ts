d0d470c1d193eb7d86b31995ba1ad07dec118aae
Ola Stenberg <hello@olastenberg.com>
2023-01-08 13:28:56 +0100

    perf(packages/router): optimise query

--- packages/router/src/lib/api.ts
+++ packages/router/src/lib/api.ts
@@ -22,137 +22,145 @@ export async function getPools(
   const token0Id = chainId.toString().concat(':').concat(token0Address.toLowerCase())
   const token1Id = chainId.toString().concat(':').concat(token1Address.toLowerCase())
 
-  const pools = await Promise.all([
-    prisma.pool.findMany({
+  const result = await Promise.all([
+    prisma.token.findFirstOrThrow({
       include: {
-        token0: true,
-        token1: true,
-      },
-      // select: {
-      //   address: true,
-      //   token0: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      //   token1: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      // },
-      where: {
-        AND: [
-          {
+        pools0: {
+          where: {
             chainId,
-            isWhitelisted: true,
             protocol,
             version,
             type: poolType,
             OR: [
               {
-                token0Id,
+                token0Id: token0Id,
+                token1: {
+                  status: 'APPROVED',
+                },
               },
               {
-                token1Id,
+                token1Id: token0Id,
+                token0: {
+                  status: 'APPROVED',
+                },
               },
             ],
           },
-        ],
-      },
-    }),
-    prisma.pool.findMany({
-      
-      include: {
-        token0: true,
-        token1: true,
-      },
-      // select: {
-      //   address: true,
-      //   token0: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      //   token1: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      // },
-      where: {
-        AND: [
-          {
+          include: {
+            token0: true,
+            token1: true,
+          },
+        },
+        pools1: {
+          where: {
             chainId,
-            isWhitelisted: true,
             protocol,
             version,
             type: poolType,
             OR: [
               {
-                token0Id,
+                token0Id: token0Id,
+                token1: {
+                  status: 'APPROVED',
+                },
               },
               {
-                token1Id,
+                token1Id: token0Id,
+                token0: {
+                  status: 'APPROVED',
+                },
               },
             ],
           },
-        ],
+          include: {
+            token0: true,
+            token1: true,
+          },
+        },
+      },
+      where: {
+        id: token0Id,
+      },
+    }),
+
+    prisma.token.findFirstOrThrow({
+      include: {
+        pools0: {
+          where: {
+            chainId,
+            protocol,
+            version,
+            type: poolType,
+            OR: [
+              {
+                token0Id: token1Id,
+                token1: {
+                  status: 'APPROVED',
+                },
+              },
+              {
+                token1Id: token1Id,
+                token0: {
+                  status: 'APPROVED',
+                },
+              },
+            ],
+          },
+          include: {
+            token0: true,
+            token1: true,
+          },
+        },
+        pools1: {
+          where: {
+            chainId,
+            protocol,
+            version,
+            type: poolType,
+            OR: [
+              {
+                token0Id: token1Id,
+                token1: {
+                  status: 'APPROVED',
+                },
+              },
+              {
+                token1Id: token1Id,
+                token0: {
+                  status: 'APPROVED',
+                },
+              },
+            ],
+          },
+          include: {
+            token0: true,
+            token1: true,
+          },
+        },
+      },
+      where: {
+        id: token1Id,
       },
     }),
     prisma.pool.findMany({
-      
       include: {
         token0: true,
         token1: true,
       },
-      // select: {
-      //   address: true,
-      //   token0: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      //   token1: {
-      //     select: {
-      //       address: true,
-      //       decimals: true,
-      //       symbol: true,
-      //       name: true,
-      //     },
-      //   },
-      // },
       where: {
-        AND: [
-          {
-            chainId,
-            isWhitelisted: true,
-            protocol,
-            version,
-            type: poolType,
-            token0Id: {
-              notIn: [token0Id, token1Id],
-            },
-            token1Id: {
-              notIn: [token0Id, token1Id],
-            },
+        AND: {
+          chainId,
+          isWhitelisted: true,
+          protocol,
+          version,
+          type: poolType,
+          token0Id: {
+            notIn: [token0Id, token1Id],
           },
-        ],
+          token1Id: {
+            notIn: [token0Id, token1Id],
+          },
+        },
       },
       take: 500,
       orderBy: {
@@ -163,7 +171,25 @@ export async function getPools(
   await prisma.$disconnect()
 
   const poolMap: Map<string, [Token, Token]> = new Map()
-  for (const pool of pools.flat()) {
+  for (const pool of [result[0].pools0, result[0].pools1, result[1].pools0, result[1].pools1].flat()) {
+    const token0 = new Token({
+      chainId,
+      address: pool.token0.address,
+      decimals: pool.token0.decimals,
+      symbol: pool.token0.symbol,
+      name: pool.token0.name,
+    })
+    const token1 = new Token({
+      chainId,
+      address: pool.token1.address,
+      decimals: pool.token1.decimals,
+      symbol: pool.token1.symbol,
+      name: pool.token1.name,
+    })
+    poolMap.set(pool.address, [token0, token1])
+  }
+
+  for (const pool of result[2].flat()) {
     const token0 = new Token({
       chainId,
       address: pool.token0.address,
