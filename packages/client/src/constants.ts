dce7f1c02127f84dbbf9bb3af818b54187157546
LufyCZ <lufy@sushi.com>
2023-08-16 13:45:58 +0000

    feat(pkgs/client): initial tokens client

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,2 +1,7 @@
 export const POOL_API =
   process.env['POOLS_API_V0_BASE_URL'] || process.env['NEXT_PUBLIC_POOLS_API_V0_BASE_URL'] || 'https://pools.sushi.com'
+
+export const TOKENS_API =
+  process.env['TOKENS_API_V0_BASE_URL'] ||
+  process.env['NEXT_PUBLIC_TOKENS_API_V0_BASE_URL'] ||
+  'https://tokens.sushi.com'
