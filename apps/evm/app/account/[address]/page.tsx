e1489b54818d45efa705bbafa57330d14b687909
matthewlilley <hello@matthewlilley.com>
2023-10-17 10:05:31 +0100

    feat: account page

---
+++ apps/evm/app/account/[address]/page.tsx
@@ -0,0 +1,8 @@
+export default async function AccountPage({ params }: { params: { address: string } }) {
+  const positions = (await fetch(`http://localhost:3000/pool/api/v1/positions/v2?user=${params.address}`, {
+    next: { revalidate: 60 },
+  }).then((data) => data.json()))
+  return <><h1>{params.address}</h1>
+  <div>{JSON.stringify(positions)}</div>
+  </>
+}
