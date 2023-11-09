85e1010f033c9cb095fa4f826ea6fb40f5101192
Ola Stenberg <hello@olastenberg.com>
2023-11-09 13:37:55 +0100

    feat(config/wagmi): remove alchemy provider

--- config/wagmi/src/providers.ts
+++ config/wagmi/src/providers.ts
@@ -1,15 +1,7 @@
 import { ChainId } from '@sushiswap/chain'
 import type { ChainProviderFn } from '@wagmi/core'
-import { alchemyProvider } from '@wagmi/core/providers/alchemy'
 import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc'
 import { publicProvider } from '@wagmi/core/providers/public'
-
-const alchemyId = process.env['ALCHEMY_ID'] || process.env['NEXT_PUBLIC_ALCHEMY_ID']
-// const infuraId = process.env['INFURA_ID'] || process.env['NEXT_PUBLIC_INFURA_ID']
-
-if (!alchemyId) {
-  throw Error('NO ALCHEMY ID SET')
-}
 
 export const allProviders: ChainProviderFn[] = [
   // jsonRpcProvider({
@@ -41,9 +33,6 @@ export const allProviders: ChainProviderFn[] = [
         return { http: 'https://rpc.ankr.com/gnosis' }
       }
     },
-  }),
-  alchemyProvider({
-    apiKey: alchemyId as string,
   }),
   publicProvider(),
 
