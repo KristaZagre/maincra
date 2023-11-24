a117e57ade1f6f91169d4025f1282252b3eaba61
LufyCZ <me@lufy.cz>
2023-11-24 16:22:00 +0000

    dprc

--- config/viem/src/index.ts
+++ config/viem/src/index.ts
@@ -382,8 +382,7 @@ export const config: Record<number, PublicClientConfig> = {
     chain: arbitrum,
     transport: fallback(
       [
-        http(`${arbitrum.rpcUrls.alchemy.http}/${alchemyId}`),
-        // http(`https://lb.drpc.org/ogrpc?network=arbitrum&dkey=${drpcId}`),
+        http(`https://lb.drpc.org/ogrpc?network=arbitrum&dkey=${drpcId}`),
       ],
       { rank: true }
     ),
